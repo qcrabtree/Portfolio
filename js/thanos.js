0dc45cbe3c0f86f3fc2e0808b3ecebd476f86e66
Quinn <qcrabtree23@gmail.com>
2019-07-18 15:00:26 -0400

    thanos image is now a gif

--- js/thanos.js
+++ js/thanos.js
@@ -25,13 +25,29 @@ function onBtnClick() {
 
   if (classHas(block.className, "div-show")) {
     block.className = classReplace(block.className, "div-show", "div-hidden", "card-img-top")
-    btn.innerHTML = "<img src=\"./new assets/gauntlet2.jpeg\" width=\"610px\" height=\"352px\">";
+    btn.innerHTML = "<img src=\"./new assets/snap gif.gif\" width=\"610px\" height=\"344px\">";
     const content = block.innerHTML;
     this.audio.play();
     overlay.innerHTML = "<div class=\"div-overlay div-overlay-left\">" + content + "</div>" + "<div class=\"div-overlay div-overlay-right\">" + content + "</div>" + "<div class=\"card-img-top\">" + "</div>";
   } else {
     block.className = classReplace(block.className, "div-hidden", "div-show", "card-img-top")
-    btn.innerHTML = "<img src=\"./new assets/gauntlet2.jpeg\" width=\"610px\" height=\"352px\">";
+    btn.innerHTML = "<img src=\"./new assets/time.gif\" width=\"610px\" height=\"344px\">";
     overlay.innerHTML = null;
   }
 }
+
+
+
+
+// $(document).ready(function(){
+//   $(".playbutton").click(function(){
+//    $("img").attr("src","./new assets/snap gif.gif");
+//   });
+
+//   $(".pausebutton").click(function(){
+//    $("img").attr("src","JPG IMAGE SOURCE");
+//   });
+//  });
+
+
+
